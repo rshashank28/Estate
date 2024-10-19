a242ab7a54fc2622241e34aeeb42b4b76a839984
Shashank Raj <rshashank608@gmail.com>
2024-10-19 19:06:53 +0530

    second commit

--- api/index.js
+++ api/index.js
@@ -1,7 +1,21 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js'
+
+dotenv.config();
+
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('connected to mongodb')
+}).catch((err)=>{
+    console.log(err)
+})
 
 const app= express();
 
 app.listen(3000,()=>{
-    console.log('apprun on 3000')
+    console.log('app run on 3000')
 })
+
+app.use('/api/user',userRouter)
